c00a54acbdb611cea81c6cc1d865ce800562b8ab
Jose CM <jose.coego@gmail.com>
2025-02-12 18:50:43 +0100

    feat: Add style components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Button from "./components/Styles/Button";
 
 const App = () => {
-  return <h1>¡Hola, React!</h1>;
+  return (
+    <h1>
+      ¡Hola, React!
+      <br />
+      <Button textColor="black" onClick={() => alert("click")}>
+        Click Here{" "}
+      </Button>
+    </h1>
+  );
 };
 
 export default App;
---
+++ src/components/Styles/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button.withConfig({
+  shouldForwardProp: (prop) =>
+    !["bgColor", "textColor", "hoverColor"].includes(prop),
+})`
+  background-color: ${(props) => props.bgColor || "#3498db"};
+  color: ${(props) => props.textColor || "white"};
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    background-color: ${(props) => props.hoverColor || "#2980b9"};
+  }
+`;
+
+export default Button;
